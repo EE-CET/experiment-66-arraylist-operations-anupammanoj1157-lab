b2c5df8f81b9a334b14c5715fc02911f125c3454
anupammanoj1157-lab <anupammanoj1157@gmail.com>
2026-04-10 21:12:52 +0530

    completed ArrayListOperations

--- src/ArrayListOperations.java
+++ src/ArrayListOperations.java
@@ -2,9 +2,29 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
 
-public class ArrayListOperations {
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+           int n = Integer.parseInt(scanner.nextLine());
+        String[] items = scanner.nextLine().split(" ");
+
+        ArrayList<String> list = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            list.add(items[i]);
+        }
+        Collections.sort(list);
+
+        String searchItem = scanner.nextLine();
+
+        System.out.println("Sorted Items: " + list);
+
+        if (list.contains(searchItem)) {
+            System.out.print("Found");
+        } else {
+            System.out.print("Not Found");
+        }
     }
 }
+    
+
